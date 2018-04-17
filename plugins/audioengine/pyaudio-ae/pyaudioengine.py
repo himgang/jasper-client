7b6fcc0a7a4a701707180c3d841679311ca6b642
himgang <himanshu.gangwar@timesinternet.in>
2018-04-18 00:46:37 +0530

    **

--- plugins/audioengine/pyaudio-ae/pyaudioengine.py
+++ plugins/audioengine/pyaudio-ae/pyaudioengine.py
@@ -156,7 +156,7 @@ class PyAudioDevice(plugin.audioengine.AudioDevice):
             'output': output,
             'input': not output,
             ('%s_device_index' % direction): self.index,
-            'frames_per_buffer': chunksize if output else chunksize*8  # Hacky
+            'frames_per_buffer': chunksize if output else chunksize*20  # Hacky
         }
         stream = self._engine._pyaudio.open(**stream_kwargs)
 
