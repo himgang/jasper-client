6e9c62d7f336b5592261a68d982e579f0572ca8a
himgang <himanshu.gangwar@timesinternet.in>
2018-04-18 01:20:06 +0530

    **

--- jasper/mic.py
+++ jasper/mic.py
@@ -166,7 +166,7 @@ class Mic(object):
                 frames.append(frame)
                 if not recording:
                     snr = self._snr([frame])
-                    if snr >= 6:  # 6dB
+                    if snr >= 9:  # 9dB
                         # Loudness is higher than normal, start recording and use
                         # the last 10 frames to start
                         self._logger.debug("Started recording on device '%s'",
